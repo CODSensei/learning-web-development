7accf69f8ab1c84a3c626b9ed585da45837afc31
CODSensei <krishna2342005@gmail.com>
2024-04-01 13:47:27 +0530

    JS1-lecture

---
+++ first.js
@@ -0,0 +1,17 @@
+const product = {
+  name: "Parker Jotter Standard  CT Ball Pen (Black)",
+  rating: 4,
+  offer: 5,
+  price: 270,
+};
+
+console.log(product);
+
+const profile = {
+  user: "Shradhakhapra",
+  isFollow: false,
+  posts: 195,
+  followers: 569000,
+  following: 4,
+};
+console.log(profile);
