f4b77fb869c7b994bda03aeffb1c58802f6f7a90
CODSensei <krishna2342005@gmail.com>
2024-04-26 00:03:39 +0530

    idk

--- first.js
+++ first.js
@@ -1,25 +1,3 @@
-a = prompt("Enter a Number ");
-if (a % 5 === 0) {
-  console.log("Multiple of 5");
-} else {
-  console.log("Not Multiple of 5");
-}
- 
-
-b=prompt("Enter your Marks = ");
-
-if (b>=80 && b<=100){
-  console.log("A");
-}
-else if (b>=70 && b<=79){
-  console.log("B");
-}
-else if (b>=60 && b<=69){
-  console.log("C");
-}
-else if (b>=50 && b<=59){
-  console.log("D");
-}
-else{
-  console.log("F")
+for (let i =0 ; i<=100 ; i=i+2){
+  console.log(i);
 }
